8e0b32b57a90de1254da75ef66da205eea8d6e9d
jj1ne2 <jj1ne2o0o@gmail.com>
2024-04-03 14:56:01 +0900

    test 폴더

---
+++ php_practice/test04_html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>HTML with PHP</title>
+</head>
+<body>
+  <h3>html 연습</h3>
+  <?php
+    echo "출력 연습<br>";
+
+    $name = "홍길동";
+    echo "이름은 {$name}이다";
+  ?>
+</body>
+</html>
